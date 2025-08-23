afc0cbb2a4d4a55bcd8cb6aeec06929b6a594349
RahulD-at-592788499043 <rahul.devsoth@techwave.com>
2025-08-23 22:58:44 +0530

    added java

---
+++ src/main/java/com/example/demo/SpringbootCiCdWithGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCiCdWithGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCiCdWithGithubActionsApplication.class, args);
+	}
+
+}
